4d720f8b5d96c8bfc75ea17e5db4fc65ca0b0f42
beruka-sachin <berukasachins@gmail.com>
2024-02-13 12:12:45 +0530

    Update TasksRequestsDTO.java

--- src/main/java/com/ncinga/timer/dtos/requestDto/TasksRequestsDTO.java
+++ src/main/java/com/ncinga/timer/dtos/requestDto/TasksRequestsDTO.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TasksRequestsDTO {
-    private String email_id;
+    private String email;
     private String module;
 
 }
